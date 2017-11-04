1862a7ac74d83d88f3cfec0ef0a381bb9642a153
Fen0l <hello@anthonypradal.com>
2017-11-04 13:42:05 +0100

    add loading page

--- main.js
+++ main.js
@@ -61,3 +61,21 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return false
+
+  return app.makeSingleInstance(function () {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
