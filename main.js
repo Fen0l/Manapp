e5527f9a01610134785855c7deba90402367019a
Fen0l <hello@anthonypradal.com>
2017-11-04 01:41:44 +0100

    add table for app

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+// Auto reload for html
+require('electron-reload')(__dirname);
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+  	width: 800, 
+  	height: 600,
+  	frame: false
+  })
+
+  // remove menu
+  win.setMenu(null);
+  
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +30,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
